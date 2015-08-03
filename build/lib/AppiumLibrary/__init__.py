ca3eab012d3794245645d62daaec2978c3401c7a
chenlin <chenlin@gepingtech.com>
2015-08-03 15:20:16 +0800

    Updated to AppiumLibrary 1.3.2

--- build/lib/AppiumLibrary/__init__.py
+++ build/lib/AppiumLibrary/__init__.py
@@ -43,8 +43,10 @@ class AppiumLibrary(
     | name              | Click Element `|` name=my_element                              | Matches by @name attribute        |
     | xpath             | Click Element `|` xpath=//UIATableView/UIATableCell/UIAButton  | Matches with arbitrary XPath      |
     | class             | Click Element `|` class=UIAPickerWheel                         | Matches by class                  |
-    | accessibility_id  | Click Element `|` accessibility_id=t                           |  Accessibility options utilize.   |
-
+    | accessibility_id  | Click Element `|` accessibility_id=t                           | Accessibility options utilize.    |
+    | android           | Click Element `|` android=new UiSelector().description('Apps') | Matches by Android UI Automator   |
+    | ios               | Click Element `|` ios=.buttons().withName('Apps')              | Matches by iOS UI Automation      |
+    | css               | Click Element `|` css=.green_button                            | Matches by css in webview         |
 
 
 
--- src/AppiumLibrary/keywords/_element.py
+++ src/AppiumLibrary/keywords/_element.py
@@ -19,6 +19,143 @@ class _ElementKeywords(KeywordGroup):
         self._info("Clear text field '%s'" % (locator))
         self._element_clear_text_by_locator(locator)
 
+    def click_element(self, locator):
+        """Click element identified by `locator`.
+
+        Key attributes for arbitrary elements are `index` and `name`. See
+        `introduction` for details about locating elements.
+        """
+        self._info("Clicking element '%s'." % locator)
+        self._element_find(locator, True, True).click()
+
+    def click_button(self, index_or_name):
+        """ Click button """
+        _platform_class_dict = {'ios':'UIAButton',
+                                   'android': 'android.widget.Button'}
+        if self._is_support_platform(_platform_class_dict):
+            class_name = self._get_class(_platform_class_dict)
+            self._click_element_by_class_name(class_name, index_or_name)
+
+    def input_text(self, locator, text):
+        """Types the given `text` into text field identified by `locator`.
+
+        See `introduction` for details about locating elements.
+        """
+        self._info("Typing text '%s' into text field '%s'" % (text, locator))
+        self._element_input_text_by_locator(locator, text)
+
+    def input_password(self, locator, text):
+        """Types the given password into text field identified by `locator`.
+
+        Difference between this keyword and `Input Text` is that this keyword
+        does not log the given password. See `introduction` for details about
+        locating elements.
+        """
+        self._info("Typing password into text field '%s'" % locator)
+        self._element_input_text_by_locator(locator, text)
+
+    def input_value(self, locator, text):
+        """Sets the given value into text field identified by `locator`. This is an IOS only keyword, input value makes use of set_value
+
+        See `introduction` for details about locating elements.
+        """
+        self._info("Setting text '%s' into text field '%s'" % (text, locator))
+        self._element_input_value_by_locator(locator, text)
+
+    def hide_keyboard(self):
+        """
+        Hides the software keyboard on the device, using the specified key to
+        press. If no key name is given, the keyboard is closed by moving focus
+        from the text field. iOS only.
+        """
+        driver = self._current_application()
+        driver.hide_keyboard()
+
+    def page_should_contain_text(self, text, loglevel='INFO'):
+        """Verifies that current page contains `text`.
+
+        If this keyword fails, it automatically logs the page source
+        using the log level specified with the optional `loglevel` argument.
+        Giving `NONE` as level disables logging.
+        """
+        if not text in self.log_source(loglevel):
+            self.log_source(loglevel)
+            raise AssertionError("Page should have contained text '%s' "
+                                 "but did not" % text)
+        self._info("Current page contains text '%s'." % text)
+
+    def page_should_not_contain_text(self, text, loglevel='INFO'):
+        """Verifies that current page not contains `text`.
+
+        If this keyword fails, it automatically logs the page source
+        using the log level specified with the optional `loglevel` argument.
+        Giving `NONE` as level disables logging.
+        """
+        if text in self.log_source(loglevel):
+            self.log_source(loglevel)
+            raise AssertionError("Page should not have contained text '%s' "
+                                 "but did not" % text)
+        self._info("Current page does not contains text '%s'." % text)
+
+    def page_should_contain_element(self, locator, loglevel='INFO'):
+        """Verifies that current page contains `locator` element.
+
+        If this keyword fails, it automatically logs the page source
+        using the log level specified with the optional `loglevel` argument.
+        Givin
+        """
+        if not self._is_element_present(locator):
+            self.log_source(loglevel)
+            raise AssertionError("Page should have contained element '%s' "
+                                 "but did not" % locator)
+        self._info("Current page contains element '%s'." % locator)
+
+    def page_should_not_contain_element(self, locator, loglevel='INFO'):
+        """Verifies that current page not contains `locator` element.
+
+        If this keyword fails, it automatically logs the page source
+        using the log level specified with the optional `loglevel` argument.
+        Givin
+        """
+        if self._is_element_present(locator):
+            self.log_source(loglevel)
+            raise AssertionError("Page should not have contained element '%s' "
+                                 "but did not" % locator)
+        self._info("Current page not contains element '%s'." % locator)
+
+    def element_should_be_disabled(self, locator, loglevel='INFO'):
+        """Verifies that element identified with locator is disabled.
+
+        Key attributes for arbitrary elements are `id` and `name`. See
+        `introduction` for details about locating elements.
+        """
+        if self._element_find(locator, True, True).is_enabled():
+            self.log_source(loglevel)
+            raise AssertionError("Element '%s' should be disabled "
+                                 "but did not" % locator)
+        self._info("Element '%s' is disabled ." % locator)
+
+    def element_should_be_enabled(self, locator, loglevel='INFO'):
+        """Verifies that element identified with locator is enabled.
+
+        Key attributes for arbitrary elements are `id` and `name`. See
+        `introduction` for details about locating elements.
+        """
+        if not self._element_find(locator, True, True).is_enabled():
+            self.log_source(loglevel)
+            raise AssertionError("Element '%s' should be enabled "
+                                 "but did not" % locator)
+        self._info("Element '%s' is enabled ." % locator)
+
+
+    def element_name_should_be(self, locator, expected):
+        element = self._element_find(locator, True, True)
+        if expected != element.get_attribute('name'):
+            raise AssertionError("Element '%s' name should be '%s' "
+                                 "but it is '%s'." % (locator, expected, element.get_attribute('name')))
+        self._info("Element '%s' name is '%s' " % (locator, expected))
+
+# Add some function which is written by stevekcolin
     def get_name(self,locator):
         """get text of element identified by `locator`.
         get text of the element
@@ -163,6 +300,10 @@ class _ElementKeywords(KeywordGroup):
         str = 'master branch'
         return str
 
+    def test_return_0803002(self):
+        str = 'return 0803002'
+        return str
+
     def get_swipe_start_end(self,x,y,args='up'):
 
         driver = self._current_application()
@@ -222,146 +363,6 @@ class _ElementKeywords(KeywordGroup):
         return startX,startY,endX,endY
 
 
-    def click_element(self, locator):
-        """Click element identified by `locator`.
-
-        Key attributes for arbitrary elements are `index` and `name`. See
-        `introduction` for details about locating elements.
-        """
-        self._info("Clicking element '%s'." % locator)
-        self._element_find(locator, True, True).click()
-
-    def click_button(self, index_or_name):
-        """ Click button """
-        _platform_class_dict = {'ios':'UIAButton',
-                                   'android': 'android.widget.Button'}
-        if self._is_support_platform(_platform_class_dict):
-            class_name = self._get_class(_platform_class_dict)
-            self._click_element_by_class_name(class_name, index_or_name)
-
-    def input_text(self, locator, text):
-        """Types the given `text` into text field identified by `locator`.
-
-        See `introduction` for details about locating elements.
-        """
-        self._info("Typing text '%s' into text field '%s'" % (text, locator))
-        self._element_input_text_by_locator(locator, text)
-
-    def input_password(self, locator, text):
-        """Types the given password into text field identified by `locator`.
-
-        Difference between this keyword and `Input Text` is that this keyword
-        does not log the given password. See `introduction` for details about
-        locating elements.
-        """
-        self._info("Typing password into text field '%s'" % locator)
-        self._element_input_text_by_locator(locator, text)
-
-    def input_value(self, locator, text):
-        """Sets the given value into text field identified by `locator`. This is an IOS only keyword, input value makes use of set_value
-
-        See `introduction` for details about locating elements.
-        """
-        self._info("Setting text '%s' into text field '%s'" % (text, locator))
-        self._element_input_value_by_locator(locator, text)
-
-    def reset_application(self):
-        """ Reset application """
-        driver = self._current_application()
-        driver.reset()
-
-    def hide_keyboard(self):
-        """
-        Hides the software keyboard on the device, using the specified key to
-        press. If no key name is given, the keyboard is closed by moving focus
-        from the text field. iOS only.
-        """
-        driver = self._current_application()
-        driver.hide_keyboard()
-
-    def page_should_contain_text(self, text, loglevel='INFO'):
-        """Verifies that current page contains `text`.
-
-        If this keyword fails, it automatically logs the page source
-        using the log level specified with the optional `loglevel` argument.
-        Giving `NONE` as level disables logging.
-        """
-        if not text in self.log_source(loglevel):
-            self.log_source(loglevel)
-            raise AssertionError("Page should have contained text '%s' "
-                                 "but did not" % text)
-        self._info("Current page contains text '%s'." % text)
-
-    def page_should_not_contain_text(self, text, loglevel='INFO'):
-        """Verifies that current page not contains `text`.
-
-        If this keyword fails, it automatically logs the page source
-        using the log level specified with the optional `loglevel` argument.
-        Giving `NONE` as level disables logging.
-        """
-        if text in self.log_source(loglevel):
-            self.log_source(loglevel)
-            raise AssertionError("Page should not have contained text '%s' "
-                                 "but did not" % text)
-        self._info("Current page does not contains text '%s'." % text)
-
-    def page_should_contain_element(self, locator, loglevel='INFO'):
-        """Verifies that current page contains `locator` element.
-
-        If this keyword fails, it automatically logs the page source
-        using the log level specified with the optional `loglevel` argument.
-        Givin
-        """
-        if not self._is_element_present(locator):
-            self.log_source(loglevel)
-            raise AssertionError("Page should have contained element '%s' "
-                                 "but did not" % locator)
-        self._info("Current page contains element '%s'." % locator)
-
-    def page_should_not_contain_element(self, locator, loglevel='INFO'):
-        """Verifies that current page not contains `locator` element.
-
-        If this keyword fails, it automatically logs the page source
-        using the log level specified with the optional `loglevel` argument.
-        Givin
-        """
-        if self._is_element_present(locator):
-            self.log_source(loglevel)
-            raise AssertionError("Page should not have contained element '%s' "
-                                 "but did not" % locator)
-        self._info("Current page not contains element '%s'." % locator)
-
-    def element_should_be_disabled(self, locator, loglevel='INFO'):
-        """Verifies that element identified with locator is disabled.
-
-        Key attributes for arbitrary elements are `id` and `name`. See
-        `introduction` for details about locating elements.
-        """
-        if self._element_find(locator, True, True).is_enabled():
-            self.log_source(loglevel)
-            raise AssertionError("Element '%s' should be disabled "
-                                 "but did not" % locator)
-        self._info("Element '%s' is disabled ." % locator)
-
-    def element_should_be_enabled(self, locator, loglevel='INFO'):
-        """Verifies that element identified with locator is enabled.
-
-        Key attributes for arbitrary elements are `id` and `name`. See
-        `introduction` for details about locating elements.
-        """
-        if not self._element_find(locator, True, True).is_enabled():
-            self.log_source(loglevel)
-            raise AssertionError("Element '%s' should be enabled "
-                                 "but did not" % locator)
-        self._info("Element '%s' is enabled ." % locator)
-
-
-    def element_name_should_be(self, locator, expected):
-        element = self._element_find(locator, True, True)
-        if expected != element.get_attribute('name'):
-            raise AssertionError("Element '%s' name should be '%s' "
-                                 "but it is '%s'." % (locator, expected, element.get_attribute('name')))
-        self._info("Element '%s' name is '%s' " % (locator, expected))
 
 
     # Private
